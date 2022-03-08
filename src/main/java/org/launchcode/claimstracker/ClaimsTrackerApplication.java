4a2d27a0dae2a8aed4fde075f8e44e7cb9fad000
lalithadevi9102 <lalithadevi9102@gmail.com>
2022-03-07 22:24:54 -0500

    main

---
+++ src/main/java/org/launchcode/claimstracker/ClaimsTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.claimstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/claimstracker/ClaimsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.claimstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
